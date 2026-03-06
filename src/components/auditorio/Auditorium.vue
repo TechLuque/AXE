eb72e187f26fdb9f6445b824b9911c76656e597e
TechLuque <tech@luqueacademy.com>
2026-03-06 17:45:32 -0500

    upd boton maestria

--- src/components/auditorio/Auditorium.vue
+++ src/components/auditorio/Auditorium.vue
@@ -72,7 +72,7 @@ const closeLbsModal = () => {
             Ingresar Al Auditorio
           </a>
 
-<!--   -->
+<!--   
           <a
             href="https://www.alvaroluque.com/mentoria-axelerate"
             class="hero-btn lbs"
@@ -81,10 +81,10 @@ const closeLbsModal = () => {
           >
             Mentoría Axelerate
           </a>
+-->
 
 
-
-<!--   
+  
           <a
             href="https://cliczy.com/separacion-msta"
             class="hero-btn lbs"
@@ -93,7 +93,7 @@ const closeLbsModal = () => {
           >
             Maestría Del Dinero
           </a>
--->
+
 
 <!--  
           <a
